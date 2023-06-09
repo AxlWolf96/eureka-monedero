955349a6020f0b653b565b690e333231fcd91aa7
Eduardo <eduardo.pullo.pisco@gmail.com>
2023-06-09 07:43:59 -0500

    [ADD] creacion de proyecto base

---
+++ src/main/java/com/proyecto/monederos/MonederosApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.monederos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonederosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonederosApplication.class, args);
+	}
+
+}
